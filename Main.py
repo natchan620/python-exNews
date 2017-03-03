f49faed57e61107961a09bb0e6ce048950890f7d
natchan620 <leungkvn@gmail.com>
2017-03-03 17:43:50 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,33 +4,30 @@ import re
 
 url = "http://www.hkexnews.hk/listedco/listconews/mainindex/SEHK_LISTEDCO_DATETIME_TODAY.HTM"
 r = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data)
+html = r.text
+soup = BeautifulSoup(html)
 
 rows = soup.find_all('tr', {'class': re.compile('row*')})
 
 for row in rows:
         print(row.get_text())
-        
-        
-table = soup.find_all('table')[1]
-rows = table.find_all('tr')[2:]
 
 data = {
-    'breeds' : [],
-    'rank2013' : [],
-    'rank2012' : [],
-    'rank2008' : [],
-    'rank2003' : []
+    'time' : [],
+    'stockcode' : [],
+    'stockname' : [],
+    'headline' : [],
+    'document' : [],
+    'docurl' : []
 }
 
 for row in rows:
     cols = row.find_all('td')
-    data['breeds'].append( cols[0].get_text() )
-    data['rank2013'].append( cols[1].get_text() )
-    data['rank2012'].append( cols[2].get_text() )
+    data['time'].append( cols[0].get_text() )
+    data['stockcode'].append( cols[1].get_text() )
+    data['stockname'].append( cols[2].get_text() )
     data['rank2008'].append( cols[3].get_text() )
     data['rank2003'].append( cols[4].get_text() )
 
-dogData = pd.DataFrame( data )
-dogData.to_csv("AKC_Dog_Registrations.csv")
+newsData = pd.DataFrame( data )
+newsData.to_csv("exNews.csv")
