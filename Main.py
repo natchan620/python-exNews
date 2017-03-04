d4103f076aa2a811ae626b65e59fdf03e9fb01fe
natchan620 <leungkvn@gmail.com>
2017-03-04 22:47:54 +0800

    add scrap ID

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import requests
 import re
 import pandas as pd
@@ -6,14 +6,17 @@ import pandas as pd
 url = "http://www.hkexnews.hk/listedco/listconews/mainindex/SEHK_LISTEDCO_DATETIME_TODAY.HTM"
 r = requests.get(url)
 html = r.text
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "lxml")
 
+
+e = re.search(r'Current Date Time: ([0-9]*)', html).group(1)
+print(e)
+
+comments = soup.find_all(text=lambda text:isinstance(text, Comment))
 rows = soup.find_all('tr', {'class': re.compile('row*')})
 
-for row in rows:
-        print(row.get_text())
-
 data = {
+	'docID' : [],
     'time' : [],
     'stockcode' : [],
     'stockname' : [],
@@ -22,10 +25,15 @@ data = {
     'docurl' : []
 }
 
+for comment in comments:
+  e = re.match(r'([0-9]*)', comment.string).group(1)
+  if len(e) > 1:
+  	data['docID'].append( e )
+
 for row in rows:
     cols = row.find_all('td')
     data['time'].append( cols[0].get_text() )
-    data['stockcode'].append( cols[1].get_text() )
+    data['stockcode'].append( cols[1].get_text() )	
     data['stockname'].append( cols[2].get_text() )
     data['headline'].append( cols[3].contents[0].contents[0] )
     data['document'].append( cols[3].contents[1].contents[0] )
