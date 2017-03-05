85e4091aa96315d08ad95a826cdf3e32d60d0f2e
natchan620 <leungkvn@gmail.com>
2017-03-06 00:12:07 +0800

    More functions

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import requests
 import re
 import pandas as pd
 
+#initialise
 data = {
 	'docID' : [],
     'time' : [],
@@ -12,9 +13,17 @@ data = {
     'document' : [],
     'docurl' : []
 }
+lastDocID = 2739765
 
 urls = ['http://www.hkexnews.hk/listedco/listconews/mainindex/SEHK_LISTEDCO_DATETIME_TODAY.HTM','http://www.hkexnews.hk/listedco/listconews/mainindex/sehk_listedco_datetime_today_c.htm','http://www.hkexnews.hk/listedco/listconews/gemindex/gem_listedco_datetime_today.htm','http://www.hkexnews.hk/listedco/listconews/gemindex/gem_listedco_datetime_today_c.htm']
 
+with open('stocklist.txt') as f:
+    rawstocklist = f.read().splitlines()
+stocklist = []
+for stock in rawstocklist:
+	stocklist.append(stock.zfill(5))
+
+#load html
 for url in urls:
 	r = requests.get(url)
 	html = r.text
@@ -30,7 +39,7 @@ for url in urls:
 	for comment in comments:
 	  e = re.match(r'([0-9]*)', comment.string).group(1)
 	  if len(e) > 1:
-	  	data['docID'].append( e )
+	  	data['docID'].append( int(e) )
 
 	for row in rows:
 	    cols = row.find_all('td')
@@ -39,10 +48,18 @@ for url in urls:
 	    data['stockname'].append( cols[2].get_text() )
 	    data['headline'].append( cols[3].contents[0].contents[0] )
 	    data['document'].append( cols[3].contents[1].contents[0] )
-	    print(cols[3].contents[1].contents[0])
 	    data['docurl'].append( "http://www.hkexnews.hk"+cols[3].contents[1].attrs["href"] )
 
 
 newsData = pd.DataFrame( data )
 newsData = newsData[['docID', 'time', 'stockcode', 'stockname', 'headline', 'document', 'docurl']]
-newsData.to_csv("exNews_"+currTime+".csv")
+
+#sortdata
+newsData = newsData.sort_values(['docID'], ascending=True)
+newsDataSorted = newsData[(newsData["docID"] > lastDocID) & (newsData["stockcode"].isin(stocklist))]
+newsDataSorted.to_csv("exNews_"+currTime+".csv")
+
+#update next minID to list
+newsData = newsData.sort_values(['docID'], ascending=False)
+lastDocID= newsData['docID'].iloc[0]
+print(lastDocID)
